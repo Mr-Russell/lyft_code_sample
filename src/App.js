8442f29c88f926ff89dd98810ef2481c406fc30e
Brian Russell <bruss@brians-mbp.lan>
2021-06-21 17:49:16 -0400

    Completed MVP. Added some minor styling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useState} from 'react'
 
 function App() {
+  const [text, setText] = useState("iamyourlyftdriver")
+  const [response, setResponse] = useState("")
+  
+  function handleChange(e){
+    setText(e.target.value)
+
+  }
+
+  function submitText(e){
+    e.preventDefault()
+    axios.post("https://lyft-interview-test.glitch.me/test", {"string_to_cut": text})
+      .then(res => {
+        setResponse(res.data.return_string)
+      })
+      .catch(err => console.log({err}))
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><span>lyft</span> API Code Challenge</h1>
+
+      <form onSubmit={submitText}>
+        <label>Text:</label> &nbsp;
+        <input
+          type="text"
+          value={text}
+          onChange={handleChange}
+        />
+
+        <br />
+
+        <input type="submit" className="submitBtn"/>
+      </form>
+
+      <div className="response">
+        <h3>API Response:</h3>
+        {response ? <p className="submitted">"{response}"</p> : <p className="notSubmitted">*Click "Submit" to view response*</p>}
+      </div>
     </div>
   );
 }
